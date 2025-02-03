d771f4c66f8324e3a64ed17ab97a11cabde713f3
Nathan <144771463+Nathan-h05@users.noreply.github.com>
2025-02-02 23:39:17 -0800

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const url = require('url');
 const utils = require('./modules/utils');
 const messages = require('./lang/messages/en/en');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
