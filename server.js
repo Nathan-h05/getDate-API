c059cf99a8b547f2bc5732c87d99e937e2126c29
Nathan <144771463+Nathan-h05@users.noreply.github.com>
2025-02-03 00:54:25 -0800

    .

--- server.js
+++ server.js
@@ -6,44 +6,47 @@ const utils = require('./modules/utils');
 const messages = require('./lang/messages/en/en');
 
 const port = process.env.PORT || 5000;
-const filePath = path.join(__dirname, 'file.txt');
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
+    const trimmedPathname = parsedUrl.pathname.replace(/^\/+|\/+$/g, '');
+    const pathnameParts = trimmedPathname.split('/');
 
-    if (parsedUrl.pathname.toLowerCase() === "/getdate" || parsedUrl.pathname.toLowerCase() === "/getdate/") {
-        const name = (parsedUrl.query.name || "Guest").replace(/\/+$/, "");
+    if (pathnameParts[0].toLowerCase() === 'getdate') {
+        const name = (parsedUrl.query.name || 'Guest').replace(/\/+$/, '');
         const currentDate = utils.getDate();
         let message = messages.greeting
-            .replace("%name%", name)
-            .replace("%datetime%", currentDate);
+            .replace('%name%', name)
+            .replace('%datetime%', currentDate);
 
-        res.writeHead(200, { "Content-Type": "text/html" });
+        res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(`<p style="color:blue;">${message}</p>`);
-    } else if (parsedUrl.pathname.toLowerCase() === "/writefile" || parsedUrl.pathname.toLowerCase() === "/writefile/") {
+    } else if (pathnameParts[0].toLowerCase() === 'writefile') {
+        const text = parsedUrl.query.text || 'Default Text';
 
-        const text = parsedUrl.query.text || "Default Text";
-        
-        fs.appendFile(filePath, text + "\n", (err) => {
+        fs.appendFile(path.join(__dirname, 'file.txt'), text + '\n', (err) => {
             if (err) {
-                res.writeHead(500, { "Content-Type": "text/html" });
-                return res.end("<h3>Error writing to file</h3>");
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                return res.end('<h3>Error writing to file</h3>');
             }
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end("<h3>File written successfully</h3>");
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('<h3>File written successfully</h3>');
         });
-    } else if (parsedUrl.pathname.toLowerCase() === "/readfile" || parsedUrl.pathname.toLowerCase() === "/readfile/") {
-        fs.readFile(filePath, "utf8", (err, data) => {
+    } else if (pathnameParts[0].toLowerCase() === 'readfile' && pathnameParts.length > 1) {
+        const fileName = pathnameParts[1];
+        const filePath = path.join(__dirname, fileName);
+
+        fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
-                res.writeHead(404, { "Content-Type": "text/html" });
-                return res.end(`<h3>404 Error: ${filePath} not found</h3>`);
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                return res.end(`<h3>404 Error: ${fileName} not found</h3>`);
             }
-            res.writeHead(200, { "Content-Type": "text/html" });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(`<pre>${data}</pre>`);
         });
     } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h3>404 Not Found</h3>");
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h3>404 Not Found</h3>');
     }
 });
 
