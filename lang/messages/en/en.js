cae8d739ae5288f8d4967aab0458267e78db8b93
Nathan <144771463+Nathan-h05@users.noreply.github.com>
2025-02-02 23:31:59 -0800

    Initial commit

---
+++ lang/messages/en/en.js
@@ -0,0 +1,4 @@
+// lang/en.js
+module.exports = {
+    greeting: "Hello %name%, What a beautiful day. Server current date and time is %datetime%"
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const url = require('url');
+const utils = require('./modules/utils');
+const messages = require('./lang/messages/en/en');
+
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname.toLowerCase() === "/getdate" || parsedUrl.pathname.toLowerCase() === "/getdate/") {
+        console.log("✅ Route matched!");
+
+        const name = (parsedUrl.query.name || "Guest").replace(/\/+$/, "");
+        const currentDate = utils.getDate();
+        let message = messages.greeting
+            .replace("%name%", name)
+            .replace("%datetime%", currentDate);
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(`<p style="color:blue;">${message}</p>`);
+    } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h3>404 Not Found</h3>");
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
